c3f477cd205a9865f0a861b31da2632874fbee1d
CINAC <chuxdesign@gmail.com>
2016-01-18 15:46:00 +0800

    version 0.1.1

--- control.py
+++ control.py
@@ -1,26 +1,43 @@
 # encoding:utf-8
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, json
 from openxl import *
+from db import *
+import time
+ISOTIMEFORMAT='%Y-%m-%d %X'
 app = Flask(__name__)
 app.debug = True
 
-
+#主页的路由
 @app.route('/')
 def index():
-    package = openxl("./59146.xls")
+    package = openxl("target/ziyang20160111.xls")
     return render_template('index.html', package=package)
 
-
+#post的地址，提供映射的方法
 @app.route('/map', methods=['POST', 'GET'])
 def map():
-    # master_arr = request.values.get('master_id', 0)
     status = "failed!"
     if request.method == 'POST':
-        # master_arr = request.form.get(master_id, 0)
-        data = request.get_json()
-        print data
-        status = "success!"
-    return status
+        cur,conn = init_database()
+        log = open('target/sql_log.txt','a')
+        data = request.form.to_dict(flat=False)
+        print len(data["master_id[]"])
+        for i in range (0, len(data["master_id[]"])):
+            print i+1,data["master_id[]"][i],data["slave_id[]"][i]
+            sql = "insert into drug_mapper (target,matched) values(%s,%s)"%(data["master_id[]"][i],data["slave_id[]"][i])
+            cur.execute(sql)
+            print "执行："+sql
+            conn.commit()
+            print u"执行成功"
+            # 写入日志前一定要字符串化信息
+            string = sql+"$"+ time.strftime( ISOTIMEFORMAT, time.localtime() )+"\n"
+            log.write(string)
+        cur.close()
+        conn.close()
+        print u"断开数据库连接！"
+        log.close()
+        status = "success"
+        return json.dumps(status)
 
 if __name__ == '__main__':
     app.run(port=8080)
